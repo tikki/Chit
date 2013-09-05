9c40166c90c36fb3ce20af7a8dcec6e24b5a80a4
Gator <dont.play@no.shit>
2013-09-05 16:10:15 +0200

    fixed url matcher for some cases

--- public/js/chat/url.js
+++ public/js/chat/url.js
@@ -1,53 +1,82 @@
-define(function () {
+define(['underscore'], function (_) {
 
 /**
  * a basic URL matcher
  */
 
-function _wrapAsLink(match, offset, string) {
-	// We'll just assume that two closing parenthesis means only one belongs to the url.
-	// We'll do no counting or such non-sense.
-	var post = '';
-	if (match.slice(-2) === '))') {
-		match = match.slice(0, -1);
-		post  = ')';
-	}
-	if (match.indexOf('.') === -1) {
-		return match;
-	}
-	return '<a href="' + match + '" class="auto-link">' + match + '</a>' + post;
-}
-
-return {
+var url = {
 	allUrls: function (s) {
 		var urls = [];
-		var regex = /\b(?:http|ftp)s?:\/\/[-a-zA-Z0-9$_.+!*'(),%;:@&=#\/?~|]*[-a-zA-Z0-9$_+*'()%@&=#\/~|]/g;
+		var regex = /(\(.*?)?\b((?:(?:http|ftp)s?:\/\/|[wW]{3}.)[-a-zA-Z0-9$_.+!*'(),%;:@&=#\/?~|]*[-a-zA-Z0-9$_+*'()%@&=#\/~|])/g;
 		while (1) {
-			var url = regex.exec();
-			if (!url) {
+			var result = regex.exec(s);
+			if (!result) {
 				break;
 			}
+			// check url (all proper internet URLs contain a .)
+			var url = result[2];
 			if (url.indexOf('.') !== -1) {
-				urls.push(url);
+				// remove trailing ) if there's a (
+				if (url.slice(-1) === ')' && result[1]) {
+					url = url.slice(0, -1);
+				}
+				// result.index is where result[0] starts, not result[2]
+				var offset = result.index + result[0].length - result[2].length;
+				urls.push({url: url, index: offset});
 			}
 		}
 		return urls;
 	},
 	replaceUrlsWithHtmlLinks: function (s) {
-		// full urls
-		regex = /\b(?:http|ftp)s?:\/\/[-a-zA-Z0-9$_.+!*'(),%;:@&=#\/?~|]*[-a-zA-Z0-9$_+*'()%@&=#\/~|]/g;
-		s = s.replace(regex, _wrapAsLink);
-
-		// incomplete http-www urls
-		regex = /(?:^|[^\/])([wW]{3}.[-a-zA-Z0-9$_.+!*'(),%;:@&=#\/?~|]*[-a-zA-Z0-9$_+*'()%@&=#\/~|])/g;
-		s = s.replace(regex, '<a href="http://$1" class="auto-link">$1</a>');
-
-		// mail addresses // these are problematic and can break stuff (ftp://user@example.com)
-		// regex = /\b([-a-zA-Z0-9._]+@[-a-zA-Z0-9._]+.[a-zA-Z]{2,6})/g;
-		// s = s.replace(regex, '<a href="mailto://$1" class="auto-link">$1</a>');
-
+		var offset = 0;
+		_.each(this.allUrls(s), function (url) {
+			var urlText = url.url;
+			var index = url.index + offset;
+			var pre = s.slice(0, index);
+			var post = s.slice(index + urlText.length);
+			// add http:// to www links
+			var link = urlText;
+			if (link.slice(0, 4).toLowerCase() === 'www.') {
+				link = 'http://' + link;
+			}
+			var insert = '<a href="' + link + '" class="auto-link">' + urlText + '</a>';
+			// update offset
+			offset += insert.length - urlText.length;
+			// update string
+			s = pre + insert + post;
+		});
 		return s;
 	}
 };
+
+// test cases
+// function testAllUrls(s, should) {
+// 	if (typeof should === 'string') {
+// 		should = [should];
+// 	}
+// 	var urls = url.allUrls(s);
+// 	var fail = false;
+// 	for (var i = 0; i < urls.length; i++) {
+// 		fail = should[i] !== urls[i].url;
+// 		if (fail) break;
+// 	}
+// 	if (fail) {
+// 		console.log('fail: "' + s + '" got', urls);
+// 	} else {
+// 		console.log('success:', s);
+// 	}
+// }
+// testAllUrls('www.example.com http://www.example.com/ http://www.example.com/foo_(bar)', ['www.example.com', 'http://www.example.com/', 'http://www.example.com/foo_(bar)']);
+// testAllUrls('(www.example.com) (http://www.example.com/) (http://www.example.com/foo_(bar))', ['www.example.com', 'http://www.example.com/', 'http://www.example.com/foo_(bar)']);
+// testAllUrls('(a www.example.com) (b http://www.example.com/) (c http://www.example.com/foo_(bar))', ['www.example.com', 'http://www.example.com/', 'http://www.example.com/foo_(bar)']);
+// testAllUrls('www.example.com) http://www.example.com/) http://www.example.com/foo_(bar))', ['www.example.com)', 'http://www.example.com/)', 'http://www.example.com/foo_(bar))']);
+// testAllUrls('(www.example.com) http://www.example.com/) http://www.example.com/foo_(bar))', ['www.example.com', 'http://www.example.com/)', 'http://www.example.com/foo_(bar))']);
+// console.log(
+// 	'a <a href="http://www.example.com" class="auto-link">www.example.com</a> b   (c <a href="http://www.as" class="auto-link">http://www.as</a><script>alert("fu")</script>) (c <a href="http://www.example.com/foo_(bar)" class="auto-link">http://www.example.com/foo_(bar)</a>) as (<a href="http://www.example.com/foo_(bar))d" class="auto-link">http://www.example.com/foo_(bar))d</a> as (<a href="http://www.example.com/foo_(bar)" class="auto-link">http://www.example.com/foo_(bar)</a>) d'
+// 	===
+// 	url.replaceUrlsWithHtmlLinks('a www.example.com b   (c http://www.as<script>alert("fu")</script>) (c http://www.example.com/foo_(bar)) as (http://www.example.com/foo_(bar))d as (http://www.example.com/foo_(bar)) d')
+// );
+
+return url;
 
 });
