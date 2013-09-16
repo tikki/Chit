7abd11c32253cce7bbf97a0b5f241bfb65dc4c26
Gator <dont.play@no.shit>
2013-09-16 02:52:21 +0200

    better handling of multiple connections for a single user (= nick/sig pair)

--- public/js/chat/ui-userlist.js
+++ public/js/chat/ui-userlist.js
@@ -7,54 +7,85 @@ define(
  * @exports Userlist
  */
 
+/**
+ * Userlist manages a ul, adding to & removing users from the list.
+ * @constructor
+ * @param {HTMLEntity} ul
+ */
 function Userlist(ul) {
+	/** @public */
 	this.ul = $(ul);
-	this._users = [];
+	/** @private */
+	this._db = [];
 }
 
-function _userSort(user) {
-	return user.user.nick;
+function _userNick(dbEntry) {
+	return dbEntry.user.nick;
 }
 
+function _makeUserFilter(user) {
+	return function (e) {
+		return user.equalTo(e.user);
+	};
+}
+
+function _updateLiWithEntry(dbEntry) {
+	$(dbEntry.li).attr('title', dbEntry.count + ' connection' + (dbEntry.count > 1 ? 's' : ''));
+}
+
+/**
+ * Add a user.
+ * @param {User} user
+ */
 Userlist.prototype.add = function(user) {
 	/** @todo this looks terribly inefficient… */
+	// check if the user was already connected
+	var dbEntry = _.find(this._db, _makeUserFilter(user));
+	if (dbEntry) {
+		dbEntry.count++;
+		_updateLiWithEntry(dbEntry);
+		return $(dbEntry.li);
+	}
 	// create dom element
 	var li = $('<li>')
 		.text(user.nick)
 		.css('color', user.color);
 	// add new user
-	this._users.push({user: user, li: li[0]});
+	this._db.push({user: user, li: li[0], count: 1});
 	// rebuild sorted user view
-	var users = _.sortBy(this._users, _userSort);
+	var db = _.sortBy(this._db, _userNick);
 	this.removeAll();
-	this._users = users;
+	this._db = db;
 	// render
 	var self = this;
-	_.each(users, function (user) {
-		self.ul.append(user.li);
+	_.each(db, function (dbEntry) {
+		self.ul.append(dbEntry.li);
+		_updateLiWithEntry(dbEntry);
 	});
 	return li;
 };
 
-Userlist.prototype.remove = function(userToRemove) {
-	// remove user from internal user-list
-	var users = [];
-	var lisToRemoved = [];
-	_.each(this._users, function (e) {
-		if (!userToRemove.equalTo(e.user)) {
-			users.push(e);
-		} else {
-			lisToRemoved.push(e.li);
-		}
-	});
-	this._users = users;
-	// remove user from ul
-	$(lisToRemoved).remove();
+/**
+ * Remove a user.
+ * @param {User} user
+ */
+Userlist.prototype.remove = function(user) {
+	var userFilter = _makeUserFilter(user);
+	var dbEntry = _.find(this._db, userFilter);
+	if (dbEntry && --dbEntry.count === 0) {
+		$(dbEntry.li).remove();
+		this._db = _.reject(this._db, userFilter);
+	} else {
+		_updateLiWithEntry(dbEntry);
+	}
 };
 
+/**
+ * Clear the ul & internal db.
+ */
 Userlist.prototype.removeAll = function() {
 	this.ul.empty();
-	this._users = [];
+	this._db = [];
 };
 
 return Userlist;
